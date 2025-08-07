4a33161204027bac717d7ca39c424b13804eb4ef
yskforest <ktokyoforest@yahoo.co.jp>
2025-08-07 11:09:45 +0900

    update

--- script/make_vm.ps1
+++ script/make_vm.ps1
@@ -1,18 +1,31 @@
-$vmName = "win11-vm"
-$isoPath = ".\iso\Win11_24H2_Japanese_x64.iso"
-$vmPath = ".\vm\$vmName"
-$vhdPath = "$vmPath\$vmName.vhdx"
+param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$vmName,
+
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$isoPath,
+
+    [Parameter(Mandatory=$true, Position=2)]
+    [string]$vmPath
+)
+
+# 指定されたパス配下にVMごとのルートフォルダパスを構成
+$vmRootPath = Join-Path -Path $vmPath -ChildPath $vmName
+# Hyper-Vの標準レイアウトに従い、VHDXのパスを構成
+$vhdPath = Join-Path -Path $vmRootPath -ChildPath "Virtual Hard Disks\$($vmName).vhdx"
+
 $memory = 32GB
 $vhdSize = 500GB
 $cpuCount = 8
 
 # VM 作成
+# VMごとにファイルを格納する専用フォルダを作成し、-Path に指定します。
 New-VM -Name $vmName `
     -MemoryStartupBytes $memory `
     -Generation 2 `
     -NewVHDPath $vhdPath `
     -NewVHDSizeBytes $vhdSize `
-    -Path $vmPath
+    -Path $vmRootPath
 
 # CPUコア数の設定
 Set-VMProcessor -VMName $vmName -Count $cpuCount
